3b3e62dbd24556de8488ffe8037b92cba510773c
Wbert Adrian Castro Vera <wcastro-psl@fpenas-mac.pslcol.com.co>
2015-08-13 17:26:28 -0500

    stable android

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,46 +1,86 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope, $http) {
-    var fd = new FormData();
-    fd.append( 'dvrId', 42 );
-    $http(
-        {
-          'method': 'POST',
-          'url': 'https://stg2.vmaxlive.net/rest/dvrStream/startStream',
-          'data': fd,
-          'headers': {'Content-Type':'application/x-www-form-urlencoded'}
-        }
-        )
-        .then(function(res){
-          console.dir(res);
-          $scope.streamUrl = res.data.html5StreamUrl;
-        }, function(error) {
-          console.dir(error);
-        });
+    .controller('BusFootageCtrl', function ($scope, $stateParams, $timeout, $ionicPlatform, StreamService) {
+        var streamId;
+        $scope.isConnected = false;
+        var videoTimeout;
+        var dvrId = $stateParams.dvrId || 8;
+
+        $scope.getStream = function(){
+            StreamService.getStream(dvrId, function (data) {
+                console.log(data);
+                console.log(data.html5StreamUrl);
+
+                if(data.error) return;
+
+                $timeout(function(){
+                    $scope.isConnected = true;
+                }, 100);
+
+                streamId = data.streamId;
+                videoTimeout = $timeout(function(){
+                    $('#myVideo').attr('src',data.html5StreamUrl);
+                }, 60000);
+
+            },function (xhr, type) {
+                alert('Ajax error! ' + type);
+            });
+        };
+
+        $scope.disconectStream = function(){
+            if($scope.isConnected) {
+                $timeout.cancel(videoTimeout);
+                StreamService.disconnectStream(streamId,
+                    function (data) {
+                        console.log(data);
+                        $timeout(function(){
+                            $scope.isConnected = false;
+                        }, 100);
+                        $('#myVideo').removeAttr('src');
+                    },
+                    function (xhr, type) {
+                        alert('Ajax error! ' + type);
+                    }
+                );
+            }
+        };
+
+        StreamService.freeStream($scope);
+
+        $timeout(function(){
+            $scope.getStream();
+        }, 5000);
+
+
 
     })
 
-.controller('ChatsCtrl', function($scope, Chats) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
+    .controller('ChatsCtrl', function ($scope, Chats, Buses) {
+        // With the new view caching in Ionic, Controllers are only called
+        // when they are recreated or on app start, instead of every page change.
+        // To listen for when this page is active (for example, to refresh data),
+        // listen for the $ionicView.enter event:
+        //
+        //$scope.$on('$ionicView.enter', function(e) {
+        //});
 
-  $scope.chats = Chats.all();
-  $scope.remove = function(chat) {
-    Chats.remove(chat);
-  };
-})
+        $scope.chats = Chats.all();
+        $scope.remove = function (chat) {
+            Chats.remove(chat);
+        };
 
-.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatId);
-})
+        Buses.all().then(function(res){
+            $scope.buses = res.data;
+            console.log(res.data);
+        })
+    })
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
-});
+    .controller('ChatDetailCtrl', function ($scope, $stateParams, Chats) {
+        $scope.chat = Chats.get($stateParams.chatId);
+    })
+
+    .controller('AccountCtrl', function ($scope) {
+        $scope.settings = {
+            enableFriends: true
+        };
+    });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,111 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+    .factory('Chats', function () {
+        // Might use a resource here that returns a JSON array
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
-  }];
+        // Some fake testing data
+        var chats = [{
+            id: 0,
+            name: 'Ben Sparrow',
+            lastText: 'You on your way?',
+            face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
+        }, {
+            id: 1,
+            name: 'Max Lynx',
+            lastText: 'Hey, it\'s me',
+            face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
+        }, {
+            id: 2,
+            name: 'Adam Bradleyson',
+            lastText: 'I should buy a boat',
+            face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
+        }, {
+            id: 3,
+            name: 'Perry Governor',
+            lastText: 'Look at my mukluks!',
+            face: 'https://pbs.twimg.com/profile_images/598205061232103424/3j5HUXMY.png'
+        }, {
+            id: 4,
+            name: 'Mike Harrington',
+            lastText: 'This is wicked good ice cream.',
+            face: 'https://pbs.twimg.com/profile_images/578237281384841216/R3ae1n61.png'
+        }];
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+        return {
+            all: function () {
+                return chats;
+            },
+            remove: function (chat) {
+                chats.splice(chats.indexOf(chat), 1);
+            },
+            get: function (chatId) {
+                for (var i = 0; i < chats.length; i++) {
+                    if (chats[i].id === parseInt(chatId)) {
+                        return chats[i];
+                    }
+                }
+                return null;
+            }
+        };
+    }).factory('StreamService', function($ionicPlatform, serviceUrl){
+        var getStream = function(dvrId, success, error){
+            $.ajax({
+                url: serviceUrl + 'dvrStream/startStream',
+                data: { dvrId: dvrId },
+                type: 'POST',
+                success: success,
+                error: error
+            });
+        };
+
+
+        var disconnectStream = function(streamId, success, error){
+            $.ajax({
+                url: serviceUrl + 'dvrStream/disconnectStream',
+                type: 'POST',
+                async: false,
+                data: { 'streamId': streamId},
+                success: success,
+                error: error
+            });
+
+        };
+
+        var freeStream = function(scope){
+            scope.$on('$destroy', function(){
+                scope.disconectStream();
+                deregisterBack();
+
+            });
+
+            $ionicPlatform.onHardwareBackButton(function(){
+                scope.disconectStream();
+            });
+
+            $ionicPlatform.on('pause', function(){
+                scope.disconectStream();
+            });
+
+            var deregisterBack = $ionicPlatform.registerBackButtonAction(function(){
+                scope.disconectStream();
+            }, 501);
+
+        };
+
+        return {
+            getStream: getStream,
+            disconnectStream: disconnectStream,
+            freeStream: freeStream
+        };
+    })
+
+    .factory('Buses', function($http, serviceUrl) {
+        function all() {
+            // https://psl.vmaxlive.net/rest/dvrStream/vehicleList?vehicleType=&groupId=&vehicleId=&after=0&filtersDisplayed=false&timeStampNoCache=1439407459685
+            return $http.get(serviceUrl + 'stream/vehicleList?vehicleType=&groupId=&vehicleId=&after=0&filtersDisplayed=false&timeStampNoCache=' + (new Date()).getTime());
         }
-      }
-      return null;
-    }
-  };
-});
+
+        return {
+            all: all
+        };
+    });
